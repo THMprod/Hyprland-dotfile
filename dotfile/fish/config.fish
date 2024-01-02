804aff8f483b86092d313ee644cc2def87838b17
Thomas Hamilcaro <t.hamilcaro@cpro-sud.fr>
2024-01-02 11:48:15 +0100

    externalisation des alias pour fish

--- dotfile/fish/config.fish
+++ dotfile/fish/config.fish
@@ -1,26 +1,12 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
+#lance neofetch au démarrage du terminal
 neofetch
 
-alias grep='rg --color=auto'
-alias ls='eza --icons'
-alias ll='eza -hl --icons'
-alias la='eza -A --icons'
-alias lla='eza -lAh --icons'
-alias cat='bat --theme=base16 --style=plain --paging=never'
-alias update-arch='yay -Syyu'
-alias clean-arch='yay -Sc && yay -Yc'
-alias fix-key='sudo rm /var/lib/pacman/sync/* && sudo rm -rf /etc/pacman.d/gnupg/* && sudo pacman-key --init && sudo pacman-key --populate && sudo pacman -Sy --noconfirm archlinux-keyring'
-alias off='sudo poweroff'
-alias reboot='sudo reboot'
-alias cmatrix='cmatrix -aoC green'
-alias lf='ranger'
-#alias rm='rm -i'
-alias note='nvim ~/.note'
-alias rm='rm -v'
-alias cp='cp -v'
-alias mv='mv -v'
+#charge les alias depuis le fichier d'alias
+source ~/.config/fish/aliases.fish
 
+#lance starship pour customiser le terminal
 starship init fish | source
 end
