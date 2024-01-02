fd1a18cb9907e5a48164f12178acebcc6a4ae880
Thomas Hamilcaro <t.hamilcaro@cpro-sud.fr>
2024-01-02 15:33:35 +0100

    Rangement et classement des alias fish

--- dotfile/fish/aliases.fish
+++ dotfile/fish/aliases.fish
@@ -1,18 +1,30 @@
+#Commande de navigation systèmes revisitées et plus jolies
 alias grep='rg --color=auto'
 alias ls='eza --icons'
 alias ll='eza -hl --icons'
 alias la='eza -A --icons'
 alias lla='eza -lAh --icons'
 alias cat='bat --theme=base16 --style=plain --paging=never'
+
+#Commandes de maintenance système
 alias update-arch='yay -Syyu'
 alias clean-arch='yay -Sc && yay -Yc'
 alias fix-key='sudo rm /var/lib/pacman/sync/* && sudo rm -rf /etc/pacman.d/gnupg/* && sudo pacman-key --init && sudo pacman-key --populate && sudo pacman -Sy --noconfirm archlinux-keyring'
+
+#Pour éteindre ou redémarrer son ordinateur plus simplement
 alias off='sudo poweroff'
 alias reboot='sudo reboot'
-alias cmatrix='cmatrix -aoC green'
-alias lf='ranger'
-#alias rm='rm -i'
+
+#Petite commande pour prendre des notes rapidement dans le terminal
 alias note='nvim ~/.note'
+
+#Ajout du verbose par défaut dans les commandes systèmes
 alias rm='rm -v'
 alias cp='cp -v'
 alias mv='mv -v'
+
+#Car on ne change pas les vieilles habitudes
+alias lf='ranger'
+
+#Commande inutile
+alias cmatrix='cmatrix -aoC green'
